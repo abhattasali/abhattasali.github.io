40fdfeecde486d8077b763259ed07e1da3556654
Arun Bhattasali <abhattasali@gmail.com>
2022-01-23 17:37:23 -0500

    Updates

---
+++ precache-manifest.c0ffbab6ab582bd106195ca4b1f4c028.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f434527f31362bbadcecc26bd75f128f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48f768280a4e5652d09e",
+    "url": "/static/css/2.72d84d2e.chunk.css"
+  },
+  {
+    "revision": "edca466bc7f04a027646",
+    "url": "/static/css/main.c1674945.chunk.css"
+  },
+  {
+    "revision": "48f768280a4e5652d09e",
+    "url": "/static/js/2.5fbf8256.chunk.js"
+  },
+  {
+    "revision": "5cc78e50619945f0d276",
+    "url": "/static/js/3.d066398b.chunk.js"
+  },
+  {
+    "revision": "edca466bc7f04a027646",
+    "url": "/static/js/main.fafd3ed9.chunk.js"
+  },
+  {
+    "revision": "68c864e189c98478db6c",
+    "url": "/static/js/runtime~main.1984d768.js"
+  },
+  {
+    "revision": "1457d650e9de30ccf39418ef2561f308",
+    "url": "/static/media/fb_pfp.1457d650.png"
+  },
+  {
+    "revision": "6cc3306db214aba9eaa217091914fc67",
+    "url": "/static/media/gmail_logo.6cc3306d.png"
+  },
+  {
+    "revision": "cfddf59a2fc3f1de042cc643ffb8cacd",
+    "url": "/static/media/ig_logo.cfddf59a.png"
+  },
+  {
+    "revision": "f45a36712c086a793f9e46fea3a4fb3d",
+    "url": "/static/media/p1.f45a3671.jpg"
+  },
+  {
+    "revision": "effc2b615ece11243e231cc7b95bf369",
+    "url": "/static/media/p2.effc2b61.png"
+  },
+  {
+    "revision": "ff9e2308ae0c87d235450d1e0d824577",
+    "url": "/static/media/p3.ff9e2308.jpg"
+  },
+  {
+    "revision": "1315aa979485fdb220ae590988b86425",
+    "url": "/static/media/p5.1315aa97.png"
+  },
+  {
+    "revision": "90125cac98ca1d983d68d07d47dc0d1a",
+    "url": "/static/media/p6.90125cac.jpg"
+  },
+  {
+    "revision": "60d1f1733904f59bd6f0c68409db9108",
+    "url": "/static/media/p7.60d1f173.png"
+  },
+  {
+    "revision": "9b2ef4c70f7cae7cb1cc7d88b1483690",
+    "url": "/static/media/resume.9b2ef4c7.pdf"
+  },
+  {
+    "revision": "c6cc44d6c6fbbe1b1ccf2064c5766b10",
+    "url": "/static/media/scrollup.c6cc44d6.png"
+  },
+  {
+    "revision": "d94c1d7beff6431d7ec17a868ce33f8a",
+    "url": "/static/media/waving.d94c1d7b.svg"
+  }
+]);
